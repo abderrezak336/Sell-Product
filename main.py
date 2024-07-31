e3206ef09b7a000148b3ab9d7a9298fd7e6db4bf
Abderrezak Haddi <abderrezak.haddi@outlook.fr>
2024-08-01 00:36:45 +0100

    upload a three files

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas
+
+
+"""سلام عزيز ضرب طلة على هدو قبل ما تبدا
+basic in python
+python list and dictionary
+class in python
+read and write file in python
+pandas in python and how to modifie and delete use python pandas and read data frame"""
+
+
+
+class System:
+    def __init__(self):
+        #write your attributes here
+        pass
+
+    def add_new_product(self):
+        #add it to products csv file
+        pass
+    def modifie_product(self):
+        pass
+    def delete_product(self):
+        pass
+    def read_products(self):
+        #if you want to call function inside function just write self.checkout
+        return None
+    def decrease_qunatity(self, product_num):
+        pass
+    def checkout(self):
+        pass
+
